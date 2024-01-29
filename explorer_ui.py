4d85c3616e3299d49ad178a385c7e68de2b57632
xavier sulkowski <xaviersulkowski@gmail.com>
2024-01-29 16:57:12 +0100

    scratch of multiselect view

--- explorer_ui.py
+++ explorer_ui.py
@@ -19,7 +19,6 @@ from models.ecg import ECGContainer, ECGLead, LeadName
 from models.span import Span
 
 APP_TITTLE = "ECG explorer"
-X_ECG_GRID_IN_MS = 200
 
 
 class MainApplication(tk.Frame):
@@ -31,8 +30,11 @@ class MainApplication(tk.Frame):
         self.container: Optional[ECGContainer] = None
         self.explorer: Optional[ECGExplorer] = None
         self.leads_mapping: dict[LeadName, int] = {}
-        self.selected_lead_name = tk.StringVar()
-        self.selected_lead: Optional[ECGLead] = None
+
+        self.selected_leads_names = tk.Variable()
+        self.selected_leads_names.trace_add("write", self._on_lead_change)
+        self.selected_leads: Optional[list[ECGLead]] = None
+
         self.spans_per_lead: dict[LeadName, list[Span]] = {}
 
         self.show_processed_signal = tk.BooleanVar()
@@ -45,34 +47,22 @@ class MainApplication(tk.Frame):
 
         self.top_frame = TopFrame(self)
         self.bottom_frame = BottomFrame(self)
-        self.canvas, self.ax, self.line = self._init_canvas()
 
-        self.leads_menu = ttk.OptionMenu(
-            self,
-            self.selected_lead_name,
-            *list(self.leads_mapping.keys()),
-            command=self._on_lead_change,
-        )
+        ecg_plot_pack_config = {"fill": tk.BOTH, "side": tk.TOP, "expand": True}
+        self.ecg_plot = ECGPlotHandler.empty(self, ecg_plot_pack_config)
+
+        self.leads_frame = LeadsFrame(self)
 
         self.top_frame.pack(anchor=tk.NW)
-        self.leads_menu.pack(anchor=tk.NE, padx=20, pady=20)
-        self.canvas.get_tk_widget().pack(fill=tk.BOTH, side=tk.TOP, expand=True)
+        self.leads_frame.pack(anchor=tk.NE, padx=20, pady=20)
+        self.ecg_plot.pack(**ecg_plot_pack_config)
         self.bottom_frame.pack(anchor=tk.SE)
-
-        span_selector = SpanSelector(
-            self.ax,
-            self._select_span,
-            "horizontal",
-            useblit=True,
-            props=dict(alpha=0.5, facecolor="red"),
-            interactive=True,
-        )
 
         self.selected_span_x_coordinates = None
 
-        self.canvas.mpl_connect("key_press_event", self._handle_key_press_event)
-        self.canvas.mpl_connect("pick_event", span_selector)
-        self.canvas.mpl_connect("button_press_event", self._highlight_span)
+        # self.canvas.mpl_connect("key_press_event", self._handle_key_press_event)
+        # self.canvas.mpl_connect("axes_enter_event", self._span_selector)
+        # self.canvas.mpl_connect("button_press_event", self._highlight_span)
 
     @staticmethod
     def _do_spans_overlap(on1, off1, on2, off2):
@@ -112,28 +102,29 @@ class MainApplication(tk.Frame):
         self.spans_per_lead[self.selected_lead.label].append(span)
         self.canvas.draw()
 
-    def _on_lead_change(self, lead_name: tk.StringVar | str):
-        if isinstance(lead_name, tk.StringVar):
-            lead_name = lead_name.get()
+    def _on_lead_change(self, *_):
 
-        self._clear_annotations()
-        self.selected_lead = self._get_lead(lead_name)
-        self.selected_span_x_coordinates = None
-        self._init_plot()
-        self._draw_annotations()
+        selected_leads = [self._get_lead(lead_name) for lead_name in self.selected_leads_names.get()]
+        self.selected_leads = selected_leads
 
-    def _init_canvas(self) -> (tk.Canvas, plt.Axes, plt.Line2D):
-        fig, ax = plt.subplots()
-        (line,) = ax.plot([], [])
-        canvas = FigureCanvasTkAgg(fig, self)
-        NavigationToolbar2Tk(canvas)
-        # ax.grid()
-        return canvas, ax, line
+        self.ecg_plot.plot_multiple_leads(self.selected_leads)
+
+        # self._clear_annotations()
+        # self.selected_lead = self._get_lead(lead_name)
+        # self.selected_span_x_coordinates = None
+        # self.plotter.plot_multiple_leads(self.selected_leads)
+        # self._draw_annotations()
 
     def _get_lead(self, lead_name: str):
         return self.container.ecg_leads[self.leads_mapping[lead_name]]
 
     def load_signal(self, filename: str):
+
+        def enable_options_on_signal_load():
+            self.top_frame.activate_widgets()
+            self.bottom_frame.activate_widgets()
+            self.leads_frame.activate_widgets()
+
         self.explorer = ECGExplorer.load_from_file(filename)
         self.container = self.explorer.container
 
@@ -142,25 +133,23 @@ class MainApplication(tk.Frame):
         self.file_path = head
         self.file_name = tail.split(".")[0]
 
+        enable_options_on_signal_load()
+
         self.leads_mapping = {
             x.label: cnt for cnt, x in enumerate(self.container.ecg_leads)
         }
-        self.leads_menu.set_menu(
-            list(self.leads_mapping.values())[0], *list(self.leads_mapping.keys())
-        )
-        self.selected_lead_name.set(list(self.leads_mapping.keys())[0])
-        self.selected_lead = self._get_lead(self.selected_lead_name.get())
+
+        self.leads_frame.update_leads(self.leads_mapping)
+
+        self.selected_leads = [
+            self._get_lead(list(self.leads_mapping.keys())[0]),
+        ]
 
         self._clear_annotations()
         self.spans_per_lead = {x.label: [] for x in self.container.ecg_leads}
 
-        self.top_frame.r1.configure(state=tk.NORMAL)
-        self.top_frame.process_ecg_button.configure(state=tk.NORMAL)
-        self.bottom_frame.generate_report_button.configure(state=tk.NORMAL)
-        self.bottom_frame.save_annotations_button.configure(state=tk.NORMAL)
-        self.top_frame.load_ann_button.configure(state=tk.NORMAL)
-
-        self._init_plot()
+        self.ecg_plot.update_ecg_container(self.container)
+        self.ecg_plot.plot_multiple_leads(self.selected_leads)
 
     def process_signal(self):
         self.explorer.process()
@@ -219,46 +208,6 @@ class MainApplication(tk.Frame):
             title=APP_TITTLE, message=f"Report generated and saved to {filename.name}"
         )
 
-    def _init_plot(self):
-        lead = self.selected_lead
-
-        waveform = (
-            lead.waveform if self.show_processed_signal.get() else lead.raw_waveform
-        )
-
-        # scale to milli-volts
-        if lead.units == 'microvolt':
-            waveform = waveform / 1000
-        else:
-            RuntimeError("Unit not known")
-
-        self.line.set_data(range(len(waveform)), waveform)
-
-        x = np.arange(0, len(waveform), X_ECG_GRID_IN_MS * lead.fs / 1000)
-        x_ticks = x / lead.fs
-
-        self.ax.set_xlim(0, len(waveform))
-        self.ax.set_xticks(x)
-        self.ax.set_xticklabels(x_ticks)
-
-        y = np.arange(math.floor(min(waveform) - 0.2), math.ceil(max(waveform) + 0.2), 0.5)
-
-        self.ax.set_ylim(min(waveform), max(waveform))
-        self.ax.set_yticks(y)
-
-        self.ax.xaxis.set_minor_locator(AutoMinorLocator(5))
-
-        self.ax.yaxis.set_minor_locator(MultipleLocator(0.1))
-
-        self.ax.grid(which='major', linestyle='-', linewidth='0.5', color='red')
-        self.ax.grid(which='minor', linestyle='-', linewidth='0.5', color=(1, 0.7, 0.7))
-
-        self.ax.set_title(f"Lead name: {lead.label}")
-        self.ax.set_ylabel(f"mV")
-        self.ax.set_xlabel(f"seconds")
-
-        self.canvas.draw()
-
     def update_waveform(self):
         lead = self.selected_lead
         waveform = (
@@ -283,8 +232,10 @@ class MainApplication(tk.Frame):
         if not len(self.spans_per_lead) > 0:
             return
 
-        for span in self.spans_per_lead[self.selected_lead.label]:
-            span.set_visible(False)
+        for lead in self.selected_leads:
+            for span in self.spans_per_lead[lead.label]:
+                span.set_visible(False)
+
         self.canvas.draw()
 
     def _highlight_span(self, event: MouseEvent):
@@ -412,6 +363,11 @@ class TopFrame(tk.Frame):
 
         self.parent.canvas.draw()
 
+    def activate_widgets(self):
+        self.r1.configure(state=tk.NORMAL)
+        self.process_ecg_button.configure(state=tk.NORMAL)
+        self.load_ann_button.configure(state=tk.NORMAL)
+
 
 class BottomFrame(tk.Frame):
     def __init__(self, parent: MainApplication, *args, **kwargs):
@@ -460,6 +416,164 @@ class BottomFrame(tk.Frame):
         self.parent.explorer.save_annotations(filename.name)
 
         showinfo(title=APP_TITTLE, message=f"Annotations saved to {filename.name}")
+
+    def activate_widgets(self):
+        self.generate_report_button.configure(state=tk.NORMAL)
+        self.save_annotations_button.configure(state=tk.NORMAL)
+
+
+class LeadsFrame(tk.Frame):
+    def __init__(self, parent: MainApplication, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.leads_listbox = tk.Listbox(self, height=3, selectmode=tk.MULTIPLE, state=tk.DISABLED)
+        scrollbar = ttk.Scrollbar(
+            self,
+            orient=tk.VERTICAL,
+            command=self.leads_listbox.yview,
+        )
+        self.leads_listbox['yscrollcommand'] = scrollbar.set
+
+        self.confirm_button = tk.Button(self, text="Confirm choices", command=self._on_confirm_button_click, state=tk.DISABLED)
+
+        self.confirm_button.pack(expand=True, side=tk.BOTTOM, fill=tk.X)
+        self.leads_listbox.pack(expand=True, fill=tk.BOTH, side=tk.LEFT)
+        scrollbar.pack(side=tk.LEFT, expand=True, fill=tk.Y)
+
+    def update_leads(self, leads_mapping: dict[LeadName, int]):
+        for name, cnt in leads_mapping.items():
+            self.leads_listbox.insert(cnt, name)
+
+    def _on_confirm_button_click(self):
+        currently_selected_idx = self.leads_listbox.curselection()
+        currently_selected = [self.leads_listbox.get(i) for i in currently_selected_idx]
+
+        if not currently_selected:
+            tk.messagebox.showinfo(title=APP_TITTLE, message="No leads selected")
+            return
+
+        selected_leads = '\n-'.join(currently_selected)
+        msg = f"Currently selected leads:\n-{selected_leads}\n\nProceed?"
+
+        should_continue = tk.messagebox.askyesno(
+            title=APP_TITTLE,
+            message=msg
+        )
+
+        if should_continue:
+            self.parent.selected_leads_names.set(currently_selected)
+
+    def activate_widgets(self):
+        self.leads_listbox.configure(state=tk.NORMAL)
+        self.confirm_button.configure(state=tk.NORMAL)
+
+
+class ECGPlotHandler(tk.Frame):
+
+    X_ECG_GRID_IN_MS = 200
+
+    def __init__(self, parent, pack_config, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+
+        # ====== widgets ======
+        self.fig: plt.Figure = plt.figure()
+        self.fig.tight_layout()
+
+        self.canvas = FigureCanvasTkAgg(self.fig, self)
+        NavigationToolbar2Tk(self.canvas)
+        self.canvas.get_tk_widget().pack(**pack_config)
+
+        # ====== app variables ======
+        self.ecg_container: Optional[ECGContainer] = None
+
+        self.axes: Optional[list[plt.Axes]] = None
+        self.lines: Optional[list[plt.Line2D]] = None
+        self.span_selectors: Optional[list[SpanSelector]] = None
+
+    @classmethod
+    def empty(cls, parent: tk.Frame, pack_config: dict):
+        return ECGPlotHandler(parent, pack_config)
+
+    def update_ecg_container(self, ecg_container: ECGContainer):
+        self.ecg_container = ecg_container
+
+    def create_empty_plots(self, n_subplots: int):
+        self.fig.clear()
+
+        n_columns = 2 if n_subplots > 3 else 1
+        n_rows = math.ceil(n_subplots / n_columns)
+
+        axes = []
+        lines = []
+        span_selectors = []
+
+        for i in range(n_subplots):
+            ax = self.fig.add_subplot(n_rows, n_columns, i + 1)
+
+            if i > 0:
+                ax.sharex(axes[0])
+
+            (line,) = ax.plot([], [])
+
+            span_selector = SpanSelector(
+                ax,
+                lambda *x: print(x),
+                direction="horizontal",
+                useblit=True,
+                props=dict(alpha=0.5, facecolor="red"),
+                interactive=True,
+            )
+
+            axes.append(ax)
+            lines.append(line)
+            span_selectors.append(span_selector)
+
+        self.axes = axes
+        self.lines = lines
+        self.span_selectors = span_selectors
+
+    def plot_multiple_leads(self, leads: list[ECGLead], show_processed_signal: bool = False):
+        self.create_empty_plots(len(leads))
+
+        for lead, ax, line in zip(leads, self.axes, self.lines):
+            self.plot_waveform(lead, ax, line, show_processed_signal)
+
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack(fill=tk.BOTH, side=tk.TOP, expand=True)
+
+    def plot_waveform(self, lead: ECGLead, ax: plt.Axes, line: plt.Line2D, show_processed_signal: bool = False):
+        waveform = (
+            lead.waveform if show_processed_signal else lead.raw_waveform
+        )
+
+        # scale to milli-volts
+        if lead.units == 'microvolt':
+            waveform = waveform / 1000
+        else:
+            RuntimeError("Unit not known")
+
+        line.set_data(range(len(waveform)), waveform)
+
+        x = np.arange(0, len(waveform), self.X_ECG_GRID_IN_MS * lead.fs / 1000)
+        x_ticks = x / lead.fs
+        ax.set_xlim(0, len(waveform))
+        ax.set_xticks(x)
+        ax.set_xticklabels(x_ticks)
+
+        y = np.arange(math.floor(min(waveform) - 0.2), math.ceil(max(waveform) + 0.2), 0.5)
+        ax.set_ylim(min(waveform), max(waveform))
+        ax.set_yticks(y)
+
+        ax.xaxis.set_minor_locator(AutoMinorLocator(5))
+        ax.yaxis.set_minor_locator(MultipleLocator(0.1))
+
+        ax.grid(which='major', linestyle='-', linewidth='0.5', color='red')
+        ax.grid(which='minor', linestyle='-', linewidth='0.5', color=(1, 0.7, 0.7))
+
+        ax.set_title(f"{lead.label}", x=0.01, y=0.9, transform=ax.transAxes, ha="left")
+        ax.set_ylabel(f"mV", fontsize=10)
+        ax.set_xlabel(f"seconds", fontsize=10)
+        ax.label_outer()
 
 
 if __name__ == "__main__":
