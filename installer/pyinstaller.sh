c5e288c58faef3942038a07f13d3e5a865145e81
xavier <xaviersulkowski@gmail.com>
2024-04-27 15:51:15 +0200

    update pyinstaller script

--- installer/pyinstaller.sh
+++ installer/pyinstaller.sh
@@ -1,16 +1,65 @@
+echo "##########################"
+echo "Running PyInstaller script"
+echo "##########################"
+
+echo "Current path $(pwd)"
+
 if [ "${PWD##*/}" == "installer" ]
 then
   prefix="."
+  scriptprefix=".."
+  versionprefix="."
 else
   prefix="./installer"
+  scriptprefix="."
+  versionprefix=".."
 fi
 
-specpath="$prefix/explorer_ui.spec"
-echo "specpath:" $specpath
+echo "\n"
+echo "\n"
+
+echo "##########################"
+echo "Setting paths"
+echo "##########################"
+
 workdir="$prefix/build"
 echo "workdir:" $workdir
 distdir="$prefix/dist"
 echo "distdir:" $distdir
+specpath="$prefix/spec"
+echo "spec path:" $specpath
+versionfile="$versionprefix/version.rc"
+echo "version file:" $versionfile
+scriptpath="$scriptprefix/explorer_ui.py"
+echo "script path:" $scriptpath
 
+echo "\n"
+echo "\n"
 
-pyinstaller $specpath --distpath $distdir --workpath $workdir
+echo "##########################"
+echo "Deleting old builds"
+echo "##########################"
+
+echo "Deleting build $workdir"
+rm -rf $workdir
+echo "Deleted"
+
+echo "Deleting dist $distdir"
+rm -rf $distdir
+echo "Deleted"
+
+echo "Deleting spec $specpath"
+rm -rf $specpath
+echo "Deleted"
+
+echo "##########################"
+echo "Building  UI"
+echo "##########################"
+
+echo "Running pyinstaller -F --collect-submodules=pydicom $scriptpath --distpath $distdir --workpath $workdir --version-file $versionfile"
+
+pyinstaller -F --collect-submodules=pydicom $scriptpath --specpath $specpath --distpath $distdir --workpath $workdir --version-file $versionfile
+
+echo "##########################"
+echo "Done"
+echo "##########################"
